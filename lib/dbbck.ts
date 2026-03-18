363debfceb850445f7db0eb47cc85d9c4d20a81f
adriano santos <iastecsolucao@gmail.com>
2026-03-17 21:02:24 -0300

    atualização pagina parametrosnew COM ENDPOINT 1703-2102

---
+++ lib/dbbck.ts
@@ -0,0 +1,29 @@
+// lib/db.ts
+import { Pool } from "pg";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _pgPool: Pool | undefined;
+}
+
+const pool =
+  global._pgPool ??
+  new Pool({
+    connectionString: process.env.DATABASE_URL_VENDEDORES ,
+    // Se o seu DATABASE_URL não tem sslmode=require, habilite ssl manualmente:
+    // ssl: { rejectUnauthorized: false },
+  });
+
+if (!global._pgPool) global._pgPool = pool;
+
+export async function dbQuery<T = any>(text: string, params?: any[]) {
+  const client = await pool.connect();
+  try {
+    const res = await client.query<T>(text, params);
+    return res; // ou res.rows, se preferir
+  } finally {
+    client.release();
+  }
+}
+
+export { pool };
--- pages/api/erp/compras/stage.js
+++ pages/api/erp/compras/stage.js
@@ -1,4 +1,4 @@
-import { pool } from "../../../../lib/db";
+import { pool } from "../../../../lib/dbBCK";
 
 export default async function handler(req, res) {
   try {
